ecc67d03f46c5b9376653656dd722b03de608f0e
IvanovVasilii <94461908+IvanovVasilii@users.noreply.github.com>
2023-09-30 15:31:16 +0300

    Fix switching
    fix track develop branch from origin

--- autogit.sh
+++ autogit.sh
@@ -3,6 +3,8 @@
 cd /home/k3s/skill/skill_mod2
 #switch to proper branch
 git checkout develop
+#after pushing branch switch to track develop branch from origin. Switch it to "out" sourse
+git branch -u out/main develop
 git pull
 git push -u origin -o merge_request.create
 cd -
