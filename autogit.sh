c823b982266ce9cd49a70e5de41caf0d8280f291
IvanovVasilii <94461908+IvanovVasilii@users.noreply.github.com>
2023-09-30 17:15:05 +0300

    fix using in crontab

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Start ssh agent for crontab and add propper ssh key
+eval "$(ssh-agent -s)"
+ssh-add /home/k3s/.ssh/hub
 #change to directory with repo
 cd /home/k3s/skill/skill_mod2
 #after pushing branch switch to track develop branch from origin. Switch it to "out" sourse
@@ -7,5 +10,6 @@ git branch -u out/main develop
 git checkout develop
 git pull
 git push -u origin -o merge_request.create
+#kill ssh agent
+ssh-agent -k
 cd -
-#test4
