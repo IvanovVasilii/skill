2244f74e8e6aebef7efe4157c8d65d877b076848
IvanovVasilii <94461908+IvanovVasilii@users.noreply.github.com>
2023-09-30 15:52:49 +0300

    Update autogit.sh

--- autogit.sh
+++ autogit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #change to directory with repo
 cd /home/k3s/skill/skill_mod2
-#switch to proper branch
-git checkout develop
 #after pushing branch switch to track develop branch from origin. Switch it to "out" sourse
 git branch -u out/main develop
+#switch to proper branch
+git checkout develop
 git pull
 git push -u origin -o merge_request.create
 cd -
+#
